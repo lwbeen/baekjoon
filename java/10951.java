85764f8b2b95b9f83d134fab61c604f9ac8b9f5d
lwbeen <89376125+lwbeen@users.noreply.github.com>
2021-08-23 17:54:42 +0900

    Create 10951.java

---
+++ java/10951.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+    	Scanner sc = new Scanner(System.in);
+    	int A, B;
+    	
+    	while(sc.hasNext()) {
+    		A = sc.nextInt();
+    		B = sc.nextInt();
+    		System.out.println((A + B));
+    	}
+    }
+}
